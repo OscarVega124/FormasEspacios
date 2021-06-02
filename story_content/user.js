5111a3e9c5c5f5a7a81792704889fb63bfe9ab83
OscarVega124 <71149590+OscarVega124@users.noreply.github.com>
2021-06-01 20:53:45 -0500

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,20 +2,32 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5jIYFdwGdFI":
+      case "6jkKRumCK41":
         Script1();
         break;
-      case "5zgZYSvRlhS":
+      case "61En3JDRvVU":
         Script2();
         break;
-      case "6aw0696jLRB":
+      case "5aloJWRdFMr":
         Script3();
         break;
-      case "5rvEDnmjVBx":
+      case "61qDL2GOB77":
         Script4();
         break;
-      case "65wYoYlYrdn":
+      case "5f81lM3RSwP":
         Script5();
+        break;
+      case "6lvcjmfF5KY":
+        Script6();
+        break;
+      case "64tv0eaBJN0":
+        Script7();
+        break;
+      case "6H2Lrip8Wlq":
+        Script8();
+        break;
+      case "5iQvnNtWLYR":
+        Script9();
         break;
   }
 }
@@ -32,12 +44,43 @@ WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyYIwFZ0kRP28wWDqyGhPg3q-
 // ATTENTION: Use a comma if you use multiple Storyline variables
 storyline =
 {
- "FECHA" : new Date().toJSON().slice(0,2),
- "PAGO" : player.GetVar("pago")
+ "FECHA" : new Date().toJSON().slice(0,10),
+ "PAGO" : player.GetVar("pago"),
 }
 }
 
 function Script2()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script3()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+function Script4()
 {
   var player = GetPlayer();
  
@@ -49,11 +92,42 @@ WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyYIwFZ0kRP28wWDqyGhPg3q-
 // ATTENTION: Use a comma if you use multiple Storyline variables
 storyline =
 {
- "NOMBRE" : player.GetVar("Nom")
+ "NOMBRE" : player.GetVar("Nom"),
 }
 }
 
-function Script3()
+function Script5()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script6()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+function Script7()
 {
   var player = GetPlayer();
  
@@ -70,7 +144,7 @@ storyline =
 }
 }
 
-function Script4()
+function Script8()
 {
   var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
@@ -79,7 +153,7 @@ script.type = 'text/javascript';
 head.appendChild(script)
 }
 
-function Script5()
+function Script9()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
