f7a8d9c23f50a8284598ab57ddf2249c7d597a96
OscarVega124 <71149590+OscarVega124@users.noreply.github.com>
2021-06-16 14:23:01 -0500

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,19 +2,79 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6qYkF86zk88":
+      case "5uf0OJfWEeK":
         Script1();
         break;
-      case "6AnB5NfVMFf":
+      case "69fbZss70Xl":
         Script2();
         break;
-      case "6rJzMltn9Zq":
+      case "60zaOZjaoJM":
         Script3();
+        break;
+      case "5kaKAjIxSSi":
+        Script4();
+        break;
+      case "5a0ScAoxDkC":
+        Script5();
+        break;
+      case "6FvIO3NdIcC":
+        Script6();
         break;
   }
 }
 
 function Script1()
+{
+  var player = GetPlayer();
+
+var d = new Date();
+var o = {year:'numeric', month:'2-digit', day:'2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit'};
+
+
+//PLACE YOUR WEB APP URL
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzZqlH28OADqjsvvUzgxehqULZ6oINDCB1surI9GzIqOZS6qyOl/exec";
+ 
+// STORE ARTICULATE STORYLINE VARIABLES
+// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
+// ATTENTION: Use a comma if you use multiple Storyline variables
+storyline =
+{
+ "FECHA" : d.toLocaleDateString('en-US', o)
+}
+}
+
+function Script2()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script3()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+function Script4()
 {
   var player = GetPlayer();
 
@@ -38,7 +98,7 @@ storyline =
 }
 }
 
-function Script2()
+function Script5()
 {
   var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
@@ -47,7 +107,7 @@ script.type = 'text/javascript';
 head.appendChild(script)
 }
 
-function Script3()
+function Script6()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
