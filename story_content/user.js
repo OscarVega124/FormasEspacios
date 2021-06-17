417dbc668831cd13f6ff8d52b8f72c19e07e9f7d
OscarVega124 <71149590+OscarVega124@users.noreply.github.com>
2021-06-17 14:37:47 -0500

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,14 +2,32 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5V6zzaiBBcu":
+      case "6YXfdMjmH2D":
         Script1();
         break;
-      case "6QFEftZMS9b":
+      case "5t28Q0wCIlK":
         Script2();
         break;
-      case "6nVj9Ny3iDa":
+      case "6BY1c0axx00":
         Script3();
+        break;
+      case "5qVniZASoOO":
+        Script4();
+        break;
+      case "6d8IGEfyjc1":
+        Script5();
+        break;
+      case "6QV3Tl92QCP":
+        Script6();
+        break;
+      case "5YEaIr8ONrh":
+        Script7();
+        break;
+      case "6JU7Y9RJPHm":
+        Script8();
+        break;
+      case "6cuOryAYoBk":
+        Script9();
         break;
   }
 }
@@ -43,6 +61,109 @@ head.appendChild(script)
 }
 
 function Script3()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+ 
+//PLACE YOUR WEB APP URL
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwdI5wa1OSdoMX1tksr2OzBqoOFmZYQjs67EAO_TCCwwOk99BU/exec";
+ 
+// STORE ARTICULATE STORYLINE VARIABLES
+// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
+// ATTENTION: Use a comma if you use multiple Storyline variables
+storyline =
+{
+  "MATRICULA" : player.GetVar("Matricula"),
+  "NOMBRE" : player.GetVar("Nombre"),
+  "NIVEL ACADEMICO" : player.GetVar("NivelEstudios"),
+  "INSTITUCION" : player.GetVar("Institucion"),
+  "SEMESTRE" : player.GetVar("Semestres")
+}
+}
+
+function Script5()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script6()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
+function Script7()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+var d = new Date();
+var o = {hour:'2-digit', minute:'2-digit', second:'2-digit'};
+ 
+//PLACE YOUR WEB APP URL
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxeyh3zhG_QrgGPxlEiUzR42frYCEns61Ml7JvoEid-XZOfqG8d/exec";
+ 
+// STORE ARTICULATE STORYLINE VARIABLES
+// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
+// ATTENTION: Use a comma if you use multiple Storyline variables
+storyline =
+{
+  "DIAPOSITIVAS VISUALISADAS" : player.GetVar("VIS"),
+  "TOTAL DE DIAPOSITIVAS" : player.GetVar("TOT"),
+  "HORA SALIDA" : d.toLocaleDateString('en-US', o)
+}
+}
+
+function Script9()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
